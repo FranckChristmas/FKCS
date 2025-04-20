8b6084a62d122dc3efa683ce9623a12a7d838793
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-04-20 23:52:23 +0200

    applying nanim on all the links

--- src/js/index.js
+++ src/js/index.js
@@ -1,103 +1,119 @@
 // part to control the mouse movement attached to the different sections
 
-let emptyElement = document.querySelector('.about .empty'); 
-let titleElement = document.querySelector('.about .title'); 
+let emptyElement = document.querySelector(".about .empty");
+let titleElement = document.querySelector(".about .title");
 
-let figureElements = document.querySelectorAll('.service figure');
+let figureElements = document.querySelectorAll(".service figure");
 
-window.addEventListener('mousemove', handleMouseMove); 
+window.addEventListener("mousemove", handleMouseMove);
 
-function handleMouseMove( event ) {
-  emptyElement.style.flexBasis = event.clientX + 'px';
-  titleElement.style.flexBasis = event.clientY / 2 + 'px';
+function handleMouseMove(event) {
+  emptyElement.style.flexBasis = event.clientX + "px";
+  titleElement.style.flexBasis = event.clientY / 2 + "px";
 
-  figureElements.forEach(function(element) {
-      element.style.flexBasis = window.innerWidth - event.clientX + 'px';
-  })
-  
+  figureElements.forEach(function (element) {
+    element.style.flexBasis = window.innerWidth - event.clientX + "px";
+  });
 }
 
 // part that handle the color change of the letters in the h1 and h3 elements
 
-function makeSpans (selector) {
+function makeSpans(selector) {
   const elements = document.querySelectorAll(selector);
 
-  elements.forEach(element => {
-    let text = element.innerText.split('');
-    let spans = text.map(letter => {
-      return `<span class="random-letter">${letter}</span>`;
-    }).join('');
+  elements.forEach((element) => {
+    let text = element.innerText.split("");
+    let spans = text
+      .map((letter) => {
+        return `<span class="random-letter">${letter}</span>`;
+      })
+      .join("");
     element.innerHTML = spans;
   });
-  const colors = ['var(--othergreen)', 'var(--othergrey)', 'var(--darkblue)', 'var(--lightblue)', 'var(--darkgreen)', 'var(--yellow)'];
+  const colors = [
+    "var(--othergreen)",
+    "var(--othergrey)",
+    "var(--darkblue)",
+    "var(--lightblue)",
+    "var(--darkgreen)",
+    "var(--yellow)",
+  ];
 
-  const letters = document.querySelectorAll('.random-letter');
-  letters.forEach(letter => {
-    letter.addEventListener('mouseover', () => {
+  const letters = document.querySelectorAll(".random-letter");
+  letters.forEach((letter) => {
+    letter.addEventListener("mouseover", () => {
       const randomColor = colors[Math.floor(Math.random() * colors.length)];
       letter.style.color = randomColor;
     });
 
-    letter.addEventListener('mouseout', () => {
-      letter.style.color = '';
+    letter.addEventListener("mouseout", () => {
+      letter.style.color = "";
     });
   });
 }
-makeSpans('h1, h3');
+makeSpans("h1, h3");
 
 // part that handle the color change of the letters on the nav links
 
-const colors = ['var(--darkblue)', 'var(--lightblue)', 'var(--darkgreen)', 'var(--yellow)'];
+const colors = [
+  "var(--darkblue)",
+  "var(--lightblue)",
+  "var(--darkgreen)",
+  "var(--yellow)",
+];
 
-document.querySelectorAll('a.color').forEach(link => {
-  link.addEventListener('mouseover', () => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    link.style.color = randomColor;
+document.querySelectorAll("a.color").forEach((link) => {
+  let timeout;
+  link.addEventListener("mouseover", () => {
+    timeout = setTimeout(() => {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      link.style.color = randomColor;
+    }, 100);
   });
-  link.addEventListener('mouseout', () => {
-    link.style.color = ''; // reset
+  link.addEventListener("mouseout", () => {
+    link.style.color = ""; // reset
   });
 });
 
 // part contact button to ease the chevron animation
 
-const button = document.querySelector('.chevron-button');
-const track = document.querySelector('.chevron-track');
-const svg = document.querySelector('.chevron-svg');
-console.log('track:', track);
-console.log('svg:', svg);
-button.addEventListener('mouseenter', () => {
-    track.classList.add('active');
-  });
-
-button.addEventListener('mouseleave', () => {
-  const handleTransitionEnd = (e) => {
-  if (e.propertyName === 'opacity') {
-    track.classList.remove('active');
-    svg.removeEventListener('transitionend', handleTransitionEnd);
-  }
-};
-svg.addEventListener('transitionend', handleTransitionEnd);
+const button = document.querySelector(".chevron-button");
+const track = document.querySelector(".chevron-track");
+const svg = document.querySelector(".chevron-svg");
+console.log("track:", track);
+console.log("svg:", svg);
+button.addEventListener("mouseenter", () => {
+  track.classList.add("active");
 });
 
-//part animation of the of the text button 
+button.addEventListener("mouseleave", () => {
+  const handleTransitionEnd = (e) => {
+    if (e.propertyName === "opacity") {
+      track.classList.remove("active");
+      svg.removeEventListener("transitionend", handleTransitionEnd);
+    }
+  };
+  svg.addEventListener("transitionend", handleTransitionEnd);
+});
 
-const buttonText = document.querySelector('.button-text');
+//part animation of the of the text button
+
+const buttonText = document.querySelector(".button-text");
 const text = buttonText.textContent;
-buttonText.innerHTML = ''; // empty the content of the button text
+buttonText.innerHTML = ""; // empty the content of the button text
 
-text.split('').forEach((char) => {
-  const span = document.createElement('span');
-  span.classList.add('char');
-  span.textContent = char === ' ' ? '\u00A0' : char; // replace space with non-breaking space
+text.split("").forEach((char) => {
+  const span = document.createElement("span");
+  span.classList.add("char");
+  span.textContent = char === " " ? "\u00A0" : char; // replace space with non-breaking space
   buttonText.appendChild(span);
 
   //ading a listener for each letter
-  span.addEventListener('mouseenter', () => {
-    span.classList.add('animate');
+  span.addEventListener("mouseenter", () => {
+    span.classList.add("animate");
   });
-    // remove class after animation
-    span.addEventListener('animationend', () => {
-      span.classList.remove('animate');
-    });
-  })
+  // remove class after animation
+  span.addEventListener("animationend", () => {
+    span.classList.remove("animate");
+  });
+});
