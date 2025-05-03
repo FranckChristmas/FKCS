ae23a0a5d454da9f27db188da55fbd486cd62b3e
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-05-04 00:21:48 +0200

    finally solved the pb about the nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import { makeSpans, colorHoverEffect } from "./spans.js";
 import { chevronAnimation, animateButtonText } from "./chevronButton.js";
 import { animateVariableFont, wordQualifiers } from "./animateTitle.js";
+import { navAnimation } from "./navAnimation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   makeSpans("h1, h3");
@@ -9,4 +10,5 @@ document.addEventListener("DOMContentLoaded", () => {
   animateButtonText();
   animateVariableFont();
   wordQualifiers();
+  navAnimation();
 });
