cd7eeeb922d5b5c5e4c11fd12d88bf3293498e4b
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-05-04 22:12:16 +0200

    added all the project to highlight

--- src/js/renderProjects.js
+++ src/js/renderProjects.js
@@ -10,25 +10,23 @@ export function renderProjects(projects) {
     const context = Array.isArray(project.data.context)
       ? project.data.context.map((p) => p.text).join("<br>")
       : "";
-    const responsibilities = project.data.responsibilities || [];
+    const responsibilities = Array.isArray(project.data.responsibilities)
+      ? project.data.responsibilities.map((p) => p.text).join("<br>")
+      : "";
     const stack = Array.isArray(project.data.stack)
       ? project.data.stack.map((p) => p.text).join("<br>")
       : "";
     console.log("🔍 context brut :", project.data.context);
     console.log("🔍 context brut :", project.data.stack);
+    console.log("🔍 context brut :", project.data.responsibilities);
 
     const li = document.createElement("li");
     li.innerHTML = `<h3 class="variable-text highlight-date">${period}</h3>
             <span class="highlight-title">${client} - ${role} | ${sector}</span>
             <div class="highlight-description">
-              <p class="highlight-context"><strong>Contexte:</strong><br>${context}</p>
-              <p class="highlight-stack">Stack: ${stack}</p>
-              <p class="highlight-description">Responsabilités:</p>
-              <ul>
-                ${responsibilities
-                  .map((item) => `<li> - ${item.responsibility}</li>`)
-                  .join("")}
-              </ul>
+              <p class="highlight-context">${context}</p>
+              <p class="highlight-stack">Stack & environment: ${stack}</p>
+              <p class="highlight-description">Responsabilities : </br>   ${responsibilities}</p>
             </div>
       `;
     container.appendChild(li);
