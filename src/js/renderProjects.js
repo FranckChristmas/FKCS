9ad8126f92c722c8fbfc19f4acec1ed9b7e3b81e
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-05-04 22:49:14 +0200

    adapted the cms content to the design

--- src/js/renderProjects.js
+++ src/js/renderProjects.js
@@ -16,17 +16,28 @@ export function renderProjects(projects) {
     const stack = Array.isArray(project.data.stack)
       ? project.data.stack.map((p) => p.text).join("<br>")
       : "";
-    console.log("🔍 context brut :", project.data.context);
-    console.log("🔍 context brut :", project.data.stack);
-    console.log("🔍 context brut :", project.data.responsibilities);
+
+    const contextTitle = Array.isArray(project.data.context_title)
+      ? project.data.context_title.map((p) => p.text).join("<br>")
+      : "";
+
+    const respoTitle = Array.isArray(project.data.respo_title)
+      ? project.data.respo_title.map((p) => p.text).join("<br>")
+      : "";
+
+    const stackTitle = Array.isArray(project.data.stack_title)
+      ? project.data.stack_title.map((p) => p.text).join("<br>")
+      : "";
+
+    console.log("📦 Données titres Prismic :", project.data);
 
     const li = document.createElement("li");
     li.innerHTML = `<h3 class="variable-text highlight-date">${period}</h3>
             <span class="highlight-title">${client} - ${role} | ${sector}</span>
             <div class="highlight-description">
-              <p class="highlight-context">${context}</p>
-              <p class="highlight-stack">Stack & environment: ${stack}</p>
-              <p class="highlight-description">Responsabilities : </br>   ${responsibilities}</p>
+              <p class="highlight-context">${contextTitle} ${context}</p>
+              <p class="highlight-stack">${stackTitle} ${stack}</p>
+              <p class="highlight-description">${respoTitle}  </br>   ${responsibilities}</p>
             </div>
       `;
     container.appendChild(li);
