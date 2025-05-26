f3bc691b93bc25fa2c570696f0e4f988680dc7fa
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-05-26 19:28:56 +0200

    adding the hover aniùation

--- src/js/iconMenuAnimation.js
+++ src/js/iconMenuAnimation.js
@@ -36,3 +36,36 @@ burger.addEventListener("click", () => {
     });
   }
 });
+
+//to animate on the hover of the menu icon
+
+const line1 = document.querySelector(".line-1");
+const line2 = document.querySelector(".line-2");
+const line3 = document.querySelector(".line-3");
+
+//when hovering
+burger.addEventListener("mouseenter", () => {
+  if (!burger.classList.contains("open")) {
+    gsap.to(line1, {
+      duration: 0.2,
+      morphSVG: { shape: "M6 1L24 1" },
+    });
+    gsap.to(line2, {
+      duration: 0.2,
+      morphSVG: { shape: "M0 7L30 7" },
+    });
+    gsap.to(line3, {
+      duration: 0.3,
+      morphSVG: { shape: "M6 13H24" },
+    });
+  }
+});
+
+//when leaving the hover area
+burger.addEventListener("mouseleave", () => {
+  if (!burger.classList.contains("open")) {
+    gsap.to(line1, { duration: 0.3, morphSVG: "M1 10.1249L16.5157 1.00004" });
+    gsap.to(line2, { duration: 0.3, morphSVG: "M12 4.00024L41.1534 11.0768" });
+    gsap.to(line3, { duration: 0.3, morphSVG: "M11 9.00024H29" });
+  }
+});
