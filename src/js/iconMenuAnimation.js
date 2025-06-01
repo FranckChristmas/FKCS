bb6c5cf9b5a60e75a570befcc070b30c1e56d8b4
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-06-01 23:47:16 +0200

    adapted the animation of the icon menu momre smooth

--- src/js/iconMenuAnimation.js
+++ src/js/iconMenuAnimation.js
@@ -6,33 +6,33 @@ export function animateBurger(isOpen) {
   if (isOpen) {
     gsap.to(".line-1", {
       duration: 0.21,
-      ease: "expo.in",
+      ease: "power1.out",
       morphSVG: { shape: "M12 1L1.93715e-07 0.999999" },
     });
     gsap.to(".line-2", {
       duration: 0.22,
-      ease: "circ.in",
+      ease: "popower1.out",
       morphSVG: { shape: "M12 4L1.93715e-07 4" },
     });
     gsap.to(".line-3", {
       duration: 0.23,
-      ease: "expo.in",
+      ease: "popower1.out",
       morphSVG: { shape: "M12 7L1.93715e-07 7" },
     });
   } else {
     gsap.to(".line-1", {
       duration: 0.15,
-      ease: "expo.in",
+      ease: "popower1.out",
       morphSVG: { shape: "M12 1L1.93715e-07 0.999999" },
     });
     gsap.to(".line-2", {
       duration: 0.18,
-      ease: "circ.in",
+      ease: "popower1.out",
       morphSVG: { shape: "M15 2.5L3 2.5" },
     });
     gsap.to(".line-3", {
       duration: 0.21,
-      ease: "expo.in",
+      ease: "popower1.out",
       morphSVG: { shape: "M18 4L6 4" },
     });
   }
@@ -46,17 +46,17 @@ export function handleBurgerHover() {
     if (!burger.classList.contains("open")) {
       gsap.to(".line-1", {
         duration: 0.1,
-        ease: "circ.in",
+        ease: "power1.out",
         morphSVG: { shape: "M12 1L1.93715e-07 0.999999" },
       });
       gsap.to(".line-2", {
         duration: 0.15,
-        ease: "circ.in",
+        ease: "power1.out",
         morphSVG: { shape: "M12 2.5L1.93715e-07 2.5" },
       });
       gsap.to(".line-3", {
         duration: 0.18,
-        ease: "circ.in",
+        ease: "power1.out",
         morphSVG: { shape: "M12 4L1.93715e-07 4" },
       });
     }
@@ -66,17 +66,17 @@ export function handleBurgerHover() {
     if (!burger.classList.contains("open")) {
       gsap.to(".line-1", {
         duration: 0.2,
-        ease: "expo.in",
+        ease: "power1.out",
         morphSVG: { shape: "M12 1L1.93715e-07 0.999999" },
       });
       gsap.to(".line-2", {
         duration: 0.25,
-        ease: "circ.in",
+        ease: "power1.out",
         morphSVG: { shape: "M15 2.5L3 2.5" },
       });
       gsap.to(".line-3", {
         duration: 0.3,
-        ease: "expo.in",
+        ease: "power1.out",
         morphSVG: { shape: "M18 4L6 4" },
       });
     }
