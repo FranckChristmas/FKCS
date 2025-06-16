04c183865af92c2f4561aa9fee1fe59ecfa6a5ee
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2025-06-16 07:25:00 +0200

    adapted about description text for reponsive

--- src/js/textAnimation.js
+++ src/js/textAnimation.js
@@ -14,37 +14,51 @@ function splitTextToChars(el, className = "char") {
   });
 }
 
-const welcomeTitle = [
-  ...document.querySelectorAll(".projects-section-title[data-projects]"),
-];
+window.addEventListener(
+  "load",
+  () => {
+    const projectTitle = [
+      ...document.querySelectorAll(".projects-section-title[data-projects]"),
+    ];
 
-welcomeTitle.forEach((title) => {
-  splitTextToChars(title);
+    projectTitle.forEach((title) => {
+      splitTextToChars(title);
+      console.log(
+        "ScrollTrigger applied to:",
+        title,
+        "with text:",
+        title.textContent
+      );
 
-  const chars = title.querySelectorAll(".char");
+      const chars = title.querySelectorAll(".char");
 
-  chars.forEach((char) => gsap.set(char.parentNode, { perspective: 1000 }));
+      chars.forEach((char) => gsap.set(char.parentNode, { perspective: 1000 }));
 
-  gsap.fromTo(
-    chars,
-    {
-      "will-change": "opacity, transform",
-      opacity: 0,
-      rotateX: () => gsap.utils.random(-120, 120),
-      z: () => gsap.utils.random(-200, 200),
-    },
-    {
-      ease: "none",
-      opacity: 1,
-      rotateX: 0,
-      z: 0,
-      stagger: 0.02,
-      scrollTrigger: {
-        trigger: title,
-        start: "top 100%",
-        end: "center 50%",
-        scrub: true,
-      },
-    }
-  );
-});
+      gsap.fromTo(
+        chars,
+        {
+          "will-change": "opacity, transform",
+          opacity: 0,
+          rotateX: () => gsap.utils.random(-120, 120),
+          z: () => gsap.utils.random(-200, 200),
+        },
+        {
+          ease: "none",
+          opacity: 1,
+          rotateX: 0,
+          z: 0,
+          stagger: 0.02,
+          scrollTrigger: {
+            trigger: title,
+            start: window.innerWidth < 600 ? "top 95%" : "center 50%",
+            end: "top 40%",
+            scrub: true,
+            markers: true,
+          },
+        }
+      );
+    });
+    ScrollTrigger.refresh();
+  },
+  300
+);
